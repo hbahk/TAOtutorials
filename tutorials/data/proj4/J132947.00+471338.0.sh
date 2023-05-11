6dc3e8081bf42f101af58f8801c9280270485a4a
hbahk <bahkhyeonguk@gmail.com>
2023-05-11 15:43:15 +0900

    update

--- tutorials/data/proj4/J132947.00+471338.0.sh
+++ tutorials/data/proj4/J132947.00+471338.0.sh
@@ -4,7 +4,7 @@
 #
 # To invoke this script do:
 #
-# $ /bin/bash J132952.00+471142.0.sh
+# $ /bin/bash J132947.00+471338.0.sh
 #
 hasPhotoObj=''
 if [[ -n "${BOSS_PHOTOOBJ}" ]]; then
@@ -21,9 +21,29 @@ for cmd in swarp bzip2 wget; do
     fi
 done
 if [[ -z "${hasPhotoObj}" ]]; then
+    if [[ ! -f frame-r-003650-1-0090.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3650/1/frame-r-003650-1-0090.fits.bz2
+        bzip2 -d -c -k frame-r-003650-1-0090.fits.bz2 > frame-r-003650-1-0090.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0099.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/5/frame-r-003699-5-0099.fits.bz2
+        bzip2 -d -c -k frame-r-003699-5-0099.fits.bz2 > frame-r-003699-5-0099.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0100.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/5/frame-r-003699-5-0100.fits.bz2
+        bzip2 -d -c -k frame-r-003699-5-0100.fits.bz2 > frame-r-003699-5-0100.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0101.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/5/frame-r-003699-5-0101.fits.bz2
+        bzip2 -d -c -k frame-r-003699-5-0101.fits.bz2 > frame-r-003699-5-0101.fits
+    fi
     if [[ ! -f frame-r-003716-5-0117.fits ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/5/frame-r-003716-5-0117.fits.bz2
         bzip2 -d -c -k frame-r-003716-5-0117.fits.bz2 > frame-r-003716-5-0117.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0119.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/5/frame-r-003716-5-0119.fits.bz2
+        bzip2 -d -c -k frame-r-003716-5-0119.fits.bz2 > frame-r-003716-5-0119.fits
     fi
     if [[ ! -f frame-r-003716-6-0117.fits ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0117.fits.bz2
@@ -33,9 +53,41 @@ if [[ -z "${hasPhotoObj}" ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0119.fits.bz2
         bzip2 -d -c -k frame-r-003716-6-0119.fits.bz2 > frame-r-003716-6-0119.fits
     fi
+    if [[ ! -f frame-r-002964-1-0402.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/2964/1/frame-r-002964-1-0402.fits.bz2
+        bzip2 -d -c -k frame-r-002964-1-0402.fits.bz2 > frame-r-002964-1-0402.fits
+    fi
     if [[ ! -f frame-r-003699-6-0098.fits ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/6/frame-r-003699-6-0098.fits.bz2
         bzip2 -d -c -k frame-r-003699-6-0098.fits.bz2 > frame-r-003699-6-0098.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0114.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/5/frame-r-003716-5-0114.fits.bz2
+        bzip2 -d -c -k frame-r-003716-5-0114.fits.bz2 > frame-r-003716-5-0114.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0114.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0114.fits.bz2
+        bzip2 -d -c -k frame-r-003716-6-0114.fits.bz2 > frame-r-003716-6-0114.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0115.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0115.fits.bz2
+        bzip2 -d -c -k frame-r-003716-6-0115.fits.bz2 > frame-r-003716-6-0115.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0086.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3650/1/frame-r-003650-1-0086.fits.bz2
+        bzip2 -d -c -k frame-r-003650-1-0086.fits.bz2 > frame-r-003650-1-0086.fits
+    fi
+    if [[ ! -f frame-r-003699-6-0103.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/6/frame-r-003699-6-0103.fits.bz2
+        bzip2 -d -c -k frame-r-003699-6-0103.fits.bz2 > frame-r-003699-6-0103.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0120.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/5/frame-r-003716-5-0120.fits.bz2
+        bzip2 -d -c -k frame-r-003716-5-0120.fits.bz2 > frame-r-003716-5-0120.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0120.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0120.fits.bz2
+        bzip2 -d -c -k frame-r-003716-6-0120.fits.bz2 > frame-r-003716-6-0120.fits
     fi
     if [[ ! -f frame-r-003699-6-0099.fits ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/6/frame-r-003699-6-0099.fits.bz2
@@ -77,9 +129,52 @@ if [[ -z "${hasPhotoObj}" ]]; then
         wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/6/frame-r-003716-6-0118.fits.bz2
         bzip2 -d -c -k frame-r-003716-6-0118.fits.bz2 > frame-r-003716-6-0118.fits
     fi
+    if [[ ! -f frame-r-002964-1-0400.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/2964/1/frame-r-002964-1-0400.fits.bz2
+        bzip2 -d -c -k frame-r-002964-1-0400.fits.bz2 > frame-r-002964-1-0400.fits
+    fi
+    if [[ ! -f frame-r-002964-1-0401.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/2964/1/frame-r-002964-1-0401.fits.bz2
+        bzip2 -d -c -k frame-r-002964-1-0401.fits.bz2 > frame-r-002964-1-0401.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0089.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3650/1/frame-r-003650-1-0089.fits.bz2
+        bzip2 -d -c -k frame-r-003650-1-0089.fits.bz2 > frame-r-003650-1-0089.fits
+    fi
+    if [[ ! -f frame-r-003699-6-0097.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3699/6/frame-r-003699-6-0097.fits.bz2
+        bzip2 -d -c -k frame-r-003699-6-0097.fits.bz2 > frame-r-003699-6-0097.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0115.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3716/5/frame-r-003716-5-0115.fits.bz2
+        bzip2 -d -c -k frame-r-003716-5-0115.fits.bz2 > frame-r-003716-5-0115.fits
+    fi
+    if [[ ! -f frame-r-002964-1-0399.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/2964/1/frame-r-002964-1-0399.fits.bz2
+        bzip2 -d -c -k frame-r-002964-1-0399.fits.bz2 > frame-r-002964-1-0399.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0085.fits ]]; then
+        wget -q https://data.sdss.org/sas/dr12/env/BOSS_PHOTOOBJ/frames/301/3650/1/frame-r-003650-1-0085.fits.bz2
+        bzip2 -d -c -k frame-r-003650-1-0085.fits.bz2 > frame-r-003650-1-0085.fits
+    fi
 else
+    if [[ ! -f frame-r-003650-1-0090.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3650/1/frame-r-003650-1-0090.fits.bz2 > frame-r-003650-1-0090.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0099.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/5/frame-r-003699-5-0099.fits.bz2 > frame-r-003699-5-0099.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0100.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/5/frame-r-003699-5-0100.fits.bz2 > frame-r-003699-5-0100.fits
+    fi
+    if [[ ! -f frame-r-003699-5-0101.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/5/frame-r-003699-5-0101.fits.bz2 > frame-r-003699-5-0101.fits
+    fi
     if [[ ! -f frame-r-003716-5-0117.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/5/frame-r-003716-5-0117.fits.bz2 > frame-r-003716-5-0117.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0119.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/5/frame-r-003716-5-0119.fits.bz2 > frame-r-003716-5-0119.fits
     fi
     if [[ ! -f frame-r-003716-6-0117.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0117.fits.bz2 > frame-r-003716-6-0117.fits
@@ -87,8 +182,32 @@ else
     if [[ ! -f frame-r-003716-6-0119.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0119.fits.bz2 > frame-r-003716-6-0119.fits
     fi
+    if [[ ! -f frame-r-002964-1-0402.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/2964/1/frame-r-002964-1-0402.fits.bz2 > frame-r-002964-1-0402.fits
+    fi
     if [[ ! -f frame-r-003699-6-0098.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/6/frame-r-003699-6-0098.fits.bz2 > frame-r-003699-6-0098.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0114.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/5/frame-r-003716-5-0114.fits.bz2 > frame-r-003716-5-0114.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0114.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0114.fits.bz2 > frame-r-003716-6-0114.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0115.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0115.fits.bz2 > frame-r-003716-6-0115.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0086.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3650/1/frame-r-003650-1-0086.fits.bz2 > frame-r-003650-1-0086.fits
+    fi
+    if [[ ! -f frame-r-003699-6-0103.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/6/frame-r-003699-6-0103.fits.bz2 > frame-r-003699-6-0103.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0120.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/5/frame-r-003716-5-0120.fits.bz2 > frame-r-003716-5-0120.fits
+    fi
+    if [[ ! -f frame-r-003716-6-0120.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0120.fits.bz2 > frame-r-003716-6-0120.fits
     fi
     if [[ ! -f frame-r-003699-6-0099.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/6/frame-r-003699-6-0099.fits.bz2 > frame-r-003699-6-0099.fits
@@ -120,14 +239,35 @@ else
     if [[ ! -f frame-r-003716-6-0118.fits ]]; then
         bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/6/frame-r-003716-6-0118.fits.bz2 > frame-r-003716-6-0118.fits
     fi
+    if [[ ! -f frame-r-002964-1-0400.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/2964/1/frame-r-002964-1-0400.fits.bz2 > frame-r-002964-1-0400.fits
+    fi
+    if [[ ! -f frame-r-002964-1-0401.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/2964/1/frame-r-002964-1-0401.fits.bz2 > frame-r-002964-1-0401.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0089.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3650/1/frame-r-003650-1-0089.fits.bz2 > frame-r-003650-1-0089.fits
+    fi
+    if [[ ! -f frame-r-003699-6-0097.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3699/6/frame-r-003699-6-0097.fits.bz2 > frame-r-003699-6-0097.fits
+    fi
+    if [[ ! -f frame-r-003716-5-0115.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3716/5/frame-r-003716-5-0115.fits.bz2 > frame-r-003716-5-0115.fits
+    fi
+    if [[ ! -f frame-r-002964-1-0399.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/2964/1/frame-r-002964-1-0399.fits.bz2 > frame-r-002964-1-0399.fits
+    fi
+    if [[ ! -f frame-r-003650-1-0085.fits ]]; then
+        bzip2 -d -c -k ${BOSS_PHOTOOBJ}/frames/301/3650/1/frame-r-003650-1-0085.fits.bz2 > frame-r-003650-1-0085.fits
+    fi
 fi
 #
 # Filter = r
 #
 /bin/rm -f default.swarp
 cat > default.swarp <<EOT
-IMAGEOUT_NAME          J132952.00+471142.0-r.fits      # Output filename
-WEIGHTOUT_NAME       J132952.00+471142.0-r.weight.fits # Output weight-map filename
+IMAGEOUT_NAME          J132947.00+471338.0-r.fits      # Output filename
+WEIGHTOUT_NAME       J132947.00+471338.0-r.weight.fits # Output weight-map filename
 
 HEADER_ONLY            N               # Only a header as an output file (Y/N)?
 HEADER_SUFFIX          .head           # Filename extension for additional headers
@@ -154,10 +294,10 @@ PROJECTION_TYPE        TAN             # Any WCS projection code or NONE
 PROJECTION_ERR         0.001           # Maximum projection error (in output
                                        # pixels), or 0 for no approximation
 CENTER_TYPE            MANUAL          # MANUAL, ALL or MOST
-CENTER                       202.4695750000,       47.1952580000 # Image Center
+CENTER                       202.4465216670,       47.2273441667 # Image Center
 PIXELSCALE_TYPE        MANUAL          # MANUAL,FIT,MIN,MAX or MEDIAN
 PIXEL_SCALE            0.396000  # Pixel scale
-IMAGE_SIZE             145,145 # scale = 0.396127 arcsec/pixel
+IMAGE_SIZE             2727,2727 # scale = 0.396127 arcsec/pixel
 
 #-------------------------------- Resampling ----------------------------------
 
@@ -219,4 +359,4 @@ NTHREADS               0               # Number of simultaneous threads for
 
 EOT
 #
-swarp frame-r-003716-5-0117.fits frame-r-003716-6-0117.fits frame-r-003716-6-0119.fits frame-r-003699-6-0098.fits frame-r-003699-6-0099.fits frame-r-003699-6-0100.fits frame-r-003699-6-0101.fits frame-r-003699-6-0102.fits frame-r-003716-5-0116.fits frame-r-003716-5-0118.fits frame-r-003716-6-0116.fits frame-r-003650-1-0087.fits frame-r-003650-1-0088.fits frame-r-003716-6-0118.fits
+swarp frame-r-003650-1-0090.fits frame-r-003699-5-0099.fits frame-r-003699-5-0100.fits frame-r-003699-5-0101.fits frame-r-003716-5-0117.fits frame-r-003716-5-0119.fits frame-r-003716-6-0117.fits frame-r-003716-6-0119.fits frame-r-002964-1-0402.fits frame-r-003699-6-0098.fits frame-r-003716-5-0114.fits frame-r-003716-6-0114.fits frame-r-003716-6-0115.fits frame-r-003650-1-0086.fits frame-r-003699-6-0103.fits frame-r-003716-5-0120.fits frame-r-003716-6-0120.fits frame-r-003699-6-0099.fits frame-r-003699-6-0100.fits frame-r-003699-6-0101.fits frame-r-003699-6-0102.fits frame-r-003716-5-0116.fits frame-r-003716-5-0118.fits frame-r-003716-6-0116.fits frame-r-003650-1-0087.fits frame-r-003650-1-0088.fits frame-r-003716-6-0118.fits frame-r-002964-1-0400.fits frame-r-002964-1-0401.fits frame-r-003650-1-0089.fits frame-r-003699-6-0097.fits frame-r-003716-5-0115.fits frame-r-002964-1-0399.fits frame-r-003650-1-0085.fits
